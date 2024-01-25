48bef6d342ccb646ac99cf2cbbe9b049ade057d7
Luka Bagic <luka.bagi@gmail.com>
2024-01-25 10:14:31 +0100

    updates

--- zsh/aliases/workspace.zsh
+++ zsh/aliases/workspace.zsh
@@ -4,4 +4,6 @@ alias dotfiles="code ~/.dotfiles"
 
 alias ws="cd ~/workspace"
 alias coreline="cd ~/workspace/coreline"
+alias codilas="cd ~/workspace/codilas"
+alias prospecter="cd ~/workspace/codilas/prospecter"
 alias personal="cd ~/workspace/personal"
